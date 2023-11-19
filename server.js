3142b92b40e20b632aadf889cd8bb18a3b8f0ff5
GabrielPesch <gkpesch@gmail.com>
2023-11-19 16:04:42 -0300

    fyx typo

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.use(
 server.use(router)
 
 server.listen(3000, () => {
-    console.loog("Json Server is running")
+    console.log("Json Server is running")
 })
 
 module.exports = server;
